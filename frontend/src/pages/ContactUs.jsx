5cbfce3d838153ceda7cf8c3abf09f988abd783e
dhruvmehta2003 <dhruv2003mehta@gmail.com>
2023-11-20 01:02:26 +0530

    contactUs

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,32 +1,18 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const ContactUs = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(false);
-  const navigate = useNavigate();
+  const [mail, setMail] = useState("");
 
-  const handleRegister = async () => {
-    try {
-      // Fetch API
-      const result = await axios.post('http://localhost:8800/api/auth/register', {
-        username,
-        email,
-        password,
-      });
-      setUsername(result.data.username);
-      setEmail(result.data.email);
-      setPassword(result.data.password);
-      setError(false);
-      navigate("/");
-    } catch (error) {
-      setError(true);
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    localStorage.setItem('mailKey', JSON.stringify(mail));
+  }, [mail]);
+  const [problem, setProblem] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem('problemKey', JSON.stringify(problem));
+  }, [problem]);
+
   return (
     <>
       <div className="flex items-center justify-between px-6 md:px-[200px] py-4 bg-gray-800 text-white">
@@ -36,38 +22,24 @@ const ContactUs = () => {
     </div>
     <div className="w-full flex justify-center items-center h-[80vh] bg-gray-100">
       <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%] bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-left">Create an account</h1>
+        <h1 className="text-3xl font-bold text-left">Contact Us</h1>
         <input
-          onChange={(e) => setUsername(e.target.value)}
-          className="w-full px-4 py-2 border-2 border-gray-300 outline-0 rounded-md"
-          type="text"
-          placeholder="Enter your username"
-        />
-        <input
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setMail(e.target.value)}
           className="w-full px-4 py-2 border-2 border-gray-300 outline-0 rounded-md"
           type="text"
           placeholder="Enter your email"
         />
         <input
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => setProblem(e.target.value)}
           className="w-full px-4 py-2 border-2 border-gray-300 outline-0 rounded-md"
           type="password"
-          placeholder="Enter your password"
+          placeholder="Enter your problem"
         />
         <button
-          onClick={handleRegister}
           className="w-full px-4 py-4 text-lg font-bold text-white bg-gray-800 rounded-md hover:bg-gray-700"
         >
-          Register
+          Send
         </button>
-        {error && <h3 className="text-red-500 text-sm">Something went wrong</h3>}
-        <div className="flex justify-center items-center space-x-3 text-gray-600">
-          <p>Already have an account?</p>
-          <p className="hover:text-gray-800">
-            <Link to="/login">Login</Link>
-          </p>
-        </div>
       </div>
     </div>
   </>
