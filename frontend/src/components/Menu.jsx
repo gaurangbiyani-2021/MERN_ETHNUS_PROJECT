b05711e56180ea8912b1975fa9191db028d56822
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-11-20 00:39:24 +0530

    Update Menu.jsx

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -29,7 +29,7 @@ const handleLogout=async()=>{
     {!user &&<h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/register">Register</Link></h3>}
     {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/profile/"+user._id}>Profile</Link></h3>}
     {user &&<h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/write">Write</Link></h3>}
-    {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myblogs/"+user._id}>My blogs</Link></h3>}
+    {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myblogs/"+user._id}>My Poted News</Link></h3>}
     {user &&<h3 onClick={handleLogout} className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
 
     </div>
