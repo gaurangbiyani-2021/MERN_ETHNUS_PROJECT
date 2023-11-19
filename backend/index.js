6dad5a3fe2b34b539802cedb64c82445e3eed6e4
Gaurang Biyani <108536919+gaurangbiyani-2021@users.noreply.github.com>
2023-11-20 03:25:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());  // Use express.json() for parsing JSON bodies
 app.use(cors({origin:"https://newsify-frontend.onrender.com",credentials:true}))
 app.use(cookieParser());
-
+app.set("trust proxy", 1);
 // app.use("/images",express.static(path.join(__dirname,"/images")))
 
 
