65981eca91abf0a341f7762c76d110391b435a23
niketsuchak <niket.suchak2021@vitstudent.ac.in>
2023-11-19 22:37:44 +0530

    footer and homepost updatation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
   return (
     <>
 <div className=" flex mt-10 w-full bg-black px-8 md:px-[300px]  md:flex-row flex-col space-y-6 md:space-y-0 items-start md:justify-between text-sm md:text-md py-8 ">
-       <div className="flex flex-col text-white">
+       <div className="flex  flex-col text-white">
          <a href="/contactUs">Contact Us</a>
        </div>
        <div className="flex flex-col text-white">
          <a href="/about">About Us</a>
        </div>
     </div>
-    <p className="py-2 pb-6 text-center text-white bg-blue-500 text-sm">All @NEWS WEBAPP 2023</p>
+    <p className="py-2 pb-6 text-center text-white bg-blue-500 text-sm" >All @NEWS WEBAPP 2023</p>
     </>
     
   )
--- frontend/src/components/HomePosts.jsx
+++ frontend/src/components/HomePosts.jsx
@@ -6,14 +6,14 @@ const HomePosts = ({post}) => {
     <div className="w-full flex mt-8 space-x-4">
       {/* left */}
 
-      <div className="w-[35%] h-[200px] flex justify-center items-center">
+      <div className=" w-[35%] h-[200px] flex justify-center items-center ">
         <img src={post.photo} alt="" className="h-full w-full object-cover"/>
       </div>
 
       {/* right */}
       <div className="flex flex-col w-[65%]">
 
-        <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">{post.title}</h1>
+        <h1 className=" text-xl font-bold md:mb-2 mb-1 md:text-2xl ">{post.title}</h1>
         
         <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
           <p>@{post.username}</p>
