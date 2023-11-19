56b9d078c221f15fa395e961505c59e16d7c5b01
Nirbhay-279 <nirbhaytiwari990@gmail.com>
2023-11-19 12:50:04 +0530

    footer-updation

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
          <p>Terms of Service</p>
        </div>
     </div>
-    <p className="py-2 pb-6 text-center text-white bg-black text-sm">All rights reserved @NEWS WEBAPP 2023</p>
+    <p className="py-2 pb-6 text-center text-white bg-blue-500 text-sm">All @NEWS WEBAPP 2023</p>
     </>
     
   )
